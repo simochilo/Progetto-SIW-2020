8181ca01952c500f496fafa2bc9b450f992850be
simochilo <sim.chilosi@stud.uniroma3.it>
2020-06-12 18:38:22 +0200

    Edit profile v1

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +12,8 @@ import com.example.demo.controller.session.SessionData;
 import com.example.demo.model.Credentials;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.CredentialsService;
+import com.example.demo.services.UserService;
 import com.example.demo.validator.UserValidator;
 
 @Controller
@@ -27,6 +30,12 @@ public class UserController {
 
     @Autowired
     SessionData sessionData;
+    
+    @Autowired
+    CredentialsService credentialsService;
+    
+    @Autowired
+    UserService userService;
 
     /**
      * This method is called when a GET request is sent by the user to URL "/users/user_id".
@@ -53,7 +62,6 @@ public class UserController {
     public String me(Model model) {
         User loggedUser = sessionData.getLoggedUser();
         Credentials credentials = sessionData.getLoggedCredentials();
-        System.out.println(credentials.getPassword());
         model.addAttribute("user", loggedUser);
         model.addAttribute("credentials", credentials);
 
@@ -72,6 +80,22 @@ public class UserController {
         User loggedUser = sessionData.getLoggedUser();
         model.addAttribute("user", loggedUser);
         return "admin";
+    }
+    
+    @RequestMapping(value = { "/users/me/edit" }, method = RequestMethod.GET)
+    public String showEditForm(Model model) {
+    	model.addAttribute("user", sessionData.getLoggedUser());
+    	model.addAttribute("credentials", sessionData.getLoggedCredentials());
+    	return "edit";
+    }
+    
+    @RequestMapping(value = { "/users/me/edit" }, method = RequestMethod.POST)
+    public String edit(@ModelAttribute("user") User user,
+    				   @ModelAttribute("credentials") Credentials credentials,
+    				   Model model) {
+    	credentials.setUser(user);
+		credentialsService.saveCredentials(credentials);
+        return "home";
     }
 
 }
